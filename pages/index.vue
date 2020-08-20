4bb0d6ae63ecb55d3eca646b292567a45afc5fc4
unknown <aribahtiar@unsil.ac.id>
2020-08-20 12:49:16 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full pt-20 min-h-screen bg-theme_primary_light flex justify-center items-center ">
     <h1 class="text-4xl text-center">
-       BootWind Starter Template
+       Hi I'm Akew
     </h1>
   </div>
 </template>
